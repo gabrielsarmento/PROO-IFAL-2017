ab2db7781da5a9b7bc5f4f9a28fead1702cd2b18
Gabriel Sarmento <gabrielfs@tuta.io>
2018-01-30 14:11:07 -0300

    SQL e Diagrama do Banco

---
+++ DataBase/banco.sql
@@ -0,0 +1,10 @@
+create database projeto;
+use projeto;
+CREATE TABLE candidato (
+id BIGINT(10) AUTO_INCREMENT,
+nome VARCHAR(255),
+cpf VARCHAR(255),
+email VARCHAR(255),
+telefone VARCHAR(255),
+PRIMARY KEY (id)
+);
